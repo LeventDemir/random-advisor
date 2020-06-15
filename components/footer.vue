63e1260a09fae9ea48d914e47a31e4f5f316c641
LeventDemir <leventdemir518@gmail.com>
2020-06-15 15:26:52 +0300

    card, navbar, footer, switch components and home page created

---
+++ components/footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>© 2020 Random Advisor, All rights reserved</p>
+    </div>
+  </footer>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,62 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        random-advisor
-      </h1>
-      <h2 class="subtitle">
-        My neat Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="column">
+    <div class="columns">
+      <div class="column">
+        <app-switch v-model="filters.movie" checked>Movie</app-switch>
+      </div>
+      <div class="column has-text-centered">
+        <app-switch v-model="filters.music" checked>Music</app-switch>
+      </div>
+      <div class="column">
+        <app-switch class="is-pulled-right" v-model="filters.tvSeries" checked>Tv Series</app-switch>
+      </div>
+    </div>
+
+    <br />
+    <br />
+
+    <button
+      @click="advise = !advise"
+      class="button is-link is-rounded is-outlined is-fullwidth"
+    >Advise</button>
+
+    <br />
+    <br />
+    <br />
+
+    <div v-if="advise" class="columns">
+      <div class="column is-one-third">
+        <Card />
+      </div>
+      <div class="column is-one-third">
+        <Card />
+      </div>
+      <div class="column is-one-third">
+        <Card />
       </div>
     </div>
   </div>
 </template>
 
+
 <script>
-import Logo from '~/components/Logo.vue'
+import Switch from "@/components/switch";
+import Card from "@/components/card";
 
 export default {
   components: {
-    Logo
+    "app-switch": Switch,
+    Card
+  },
+  data() {
+    return {
+      advise: false,
+      filters: {
+        movie: true,
+        music: true,
+        tvSeries: true
+      }
+    };
   }
-}
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
